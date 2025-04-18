9f23d22163a129d4deaef2d427a69b8ac72a1d1f
Suraj Singh <158294092+Suraj5101@users.noreply.github.com>
2025-04-18 12:17:31 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,24 @@
+<footer class="bg-dark text-white py-4">
+  <div class="container text-center">
+    <p>&copy; 2025 CV Analyzer. All rights reserved.</p>
+    <p>Follow Us:</p>
+    <a href="https://facebook.com">
+  <img src="assets/images/facebook.png" alt="Logo" style="width: 50px; height: 50px;">
+</a>
+<a href="https://www.instagram.com/">
+  <img src="assets/images/insta.jpg" alt="Logo" style="width: 50px; height: 50px;">
+</a>
+<a href="https://x.com/?lang=en">
+  <img src="assets/images/x.png" alt="Logo" style="width: 50px; height: 50px;">
+</a>  </div>
+</footer>
+
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+<script>
+  // Toggle dark/light theme
+  document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('bg-dark');
+    document.body.classList.toggle('text-white');
+    this.textContent = document.body.classList.contains('bg-dark') ? '☀️' : '🌙';
+  });
+</script>
